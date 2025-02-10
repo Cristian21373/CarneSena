847e2789d8438ecc60d7e70fe5aaca9d14b0facb
Cristian Bahamon <cbahamongranados@gmail.com>
2025-02-09 20:22:18 -0500

    Se adelanta la vista del inicio en el front-end

--- Front-End/CarnetSena/proyecto/CarnetSena/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React, { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-root.render(
-<button>hola mundo</button>
-)
