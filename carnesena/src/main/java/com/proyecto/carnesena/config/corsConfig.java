5ba5c04ffdbb9ce6025738158aa5441c57a3e33e
Cristian Bahamon <cbahamongranados@gmail.com>
2025-04-22 23:08:55 -0500

    Se sube neuvo corsConfig

--- carnesena/src/main/java/com/proyecto/carnesena/config/corsConfig.java
+++ carnesena/src/main/java/com/proyecto/carnesena/config/corsConfig.java
@@ -8,25 +8,26 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class corsConfig {
-	
+
 	@Bean
-	public CorsFilter corsFilter () {
-		
+	public CorsFilter corsFilter() {
+
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration config = new CorsConfiguration();
-		
-		
-		config.addAllowedOrigin("http://127.0.0.1:5500/");
+
+		config.addAllowedOrigin("http://carnesena.online");
+		config.addAllowedOrigin("http://carnesena.online:8080"); // por si usas el puerto
+
 		config.addAllowedMethod("GET");
 		config.addAllowedMethod("POST");
 		config.addAllowedMethod("PUT");
 		config.addAllowedMethod("DELETE");
-		
+
 		config.addAllowedHeader("Authorization");
 		config.addAllowedHeader("Content-Type");
-		
+
 		source.registerCorsConfiguration("/**", config);
 		return new CorsFilter(source);
 	}
-	
+
 }
